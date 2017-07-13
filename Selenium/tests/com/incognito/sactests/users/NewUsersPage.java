2db844c52fffb25f139c1a0515d9ada686812ed9
ccastro <ccastro@172.17.2.161>
2017-07-13 18:02:13 +0100

    Segundo commit

---
+++ Selenium/tests/com/incognito/sactests/users/NewUsersPage.java
@@ -0,0 +1,42 @@
+package com.incognito.sactests.users;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class NewUsersPage {
+	
+	private final WebDriver driver;
+	
+	public NewUsersPage(WebDriver driver){
+		this.driver = driver;
+	}
+	
+	public void createUser(String loginName, String fullName, String password, String confirm, String phone, String email, String profile, String description){
+		
+		WebElement formLoginName = driver.findElement(By.name("loginName"));
+		WebElement formFullName = driver.findElement(By.name("name"));
+		WebElement formPassword = driver.findElement(By.name("password"));
+		WebElement formConfirmPassword = driver.findElement(By.name("confirmPassword"));
+		WebElement formPhoneNunber = driver.findElement(By.name("phoneNumber"));
+		WebElement formEmailAddress = driver.findElement(By.name("emailAddress"));
+		Select cbSecurityProfile = new Select(driver.findElement(By.name("securityProfile")));
+		WebElement formDescription = driver.findElement(By.name("description"));
+		
+		formLoginName.sendKeys(loginName);
+		formFullName.sendKeys(fullName);
+		formPassword.sendKeys(password);
+		formConfirmPassword.sendKeys(confirm);
+		formPhoneNunber.sendKeys(phone);
+		formEmailAddress.sendKeys(email);
+		cbSecurityProfile.selectByVisibleText(profile);
+		formDescription.sendKeys(description);
+		
+		formDescription.submit();
+		
+	
+	}
+
+	
+}
